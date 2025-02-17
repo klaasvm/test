e4761e1b348ea32af4ebbab92802e5915fa7e71e
Klaas Van Mechelen <klaasvanmechelengame@gmail.com>
2025-02-17 22:59:13 +0100

    clear repo

--- test.py
+++
@@ -1,7 +0,0 @@
-bestand_pad = "hello_world.txt"
-
-with open(bestand_pad, 'w') as bestand:
-    bestand.write("hello world")
-
-print(f"Het bestand '{bestand_pad}' is aangemaakt!")
-//api key stem via github
